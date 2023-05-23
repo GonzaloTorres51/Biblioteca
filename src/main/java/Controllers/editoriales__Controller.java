4e46b5da493f2364b707410547367a9d1fe2ca9f
Alumno <Alumno@24-113-DESKTOP>
2023-05-23 17:26:08 -0400

    actualizacion biblioteca

--- src/main/java/Controllers/editoriales__Controller.java
+++ src/main/java/Controllers/editoriales__Controller.java
@@ -30,10 +30,10 @@ public class editoriales__Controller extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException{
         response.setContentType("text/html;charset=UTF-8");
 
-        String mensaje = "";
+        String mensaje;
         String insertEditorial;
 
         try {
@@ -62,36 +62,5 @@ public class editoriales__Controller extends HttpServlet {
         }else{
             return new DAO_Editorial();
         }
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+    }   
 }
