bc940b49ebf14ce81532ab9094d24474f4cca53b
Alumno <Alumno@24-113-DESKTOP>
2023-05-23 16:18:07 -0400

    actualizacion biblioteca

--- src/main/java/Modals/CRUD_Editorial.java
+++ src/main/java/Modals/CRUD_Editorial.java
@@ -13,21 +13,20 @@ public class CRUD_Editorial {
     public String Create(Object obj){
         DAO_Editorial editorial = (DAO_Editorial) obj; //creamos un objeto de dao editorial
         Connection connection;    //ejecuta la conexion
-        PreparedStatement pst;
-        String query = "INSERT INTO TBL_EDITORIAL VALUES (?)";
-        String respuesta;
+        PreparedStatement pst;  //permiten mantener las consultas neutras sin tener en cuenta los parámetros
+        String query = "INSERT INTO TBL_EDITORIAL VALUES (?)";  //almacena la consulta
+        String respuesta;   //bariable resultante del CATCH
         
         try{
-            Class.forName(Conexion.getDriver());
-            connection = (Connection) DriverManager.getConnection(Conexion.getUrl(),Conexion.getUsuario(), Conexion.getPassword());
-            
-            pst = (PreparedStatement) connection.prepareStatement(query);
-            pst.setString(1,editorial.getVCH_NOMBRE_EDITORIAL());
-            respuesta = Integer.toString(pst.executeUpdate());
+            Class.forName(Conexion.getDriver());    //carga la clase en la máquina virtual, en este caso carga el driver
+            connection = (Connection) DriverManager.getConnection(Conexion.getUrl(),Conexion.getUsuario(), Conexion.getPassword()); //Método que nos permite establecer una conexión con la base de datos.            
+            pst = (PreparedStatement) connection.prepareStatement(query); //le pasa la coneccion y la consulta(sin parametros)
+            pst.setString(1,editorial.getVCH_NOMBRE_EDITORIAL());   //pasa los parametro en orden de izquierda a derecha (1,2,3),y el valor
+            respuesta = Integer.toString(pst.executeUpdate());  //devuelve el número de filas afectadas por la instrucción SQL
         }catch(ClassNotFoundException | SQLException ex){
             respuesta = ex.toString();
         }
         
-        return respuesta;
+        return respuesta;   //resultado de la consulta (funcione o no)
     }
 }
